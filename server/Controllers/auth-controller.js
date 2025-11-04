a38d0e0c4f12eb3b5d8162f54d3b276656b2dde0
Akashmesh <akashmeshram2486@gmail.com>
2025-11-04 23:58:51 +0530

    getting user data as json response

--- server/Controllers/auth-controller.js
+++ server/Controllers/auth-controller.js
@@ -9,7 +9,7 @@ const home = async(req,res) => {
 
 const register = async(req,res) => {
     try {
-        res.status(200).send("welcome to registration page from controller");
+        res.status(200).json({msg : req.body});
     } catch (error) {
         res.status(400).send({msg : "page not found"});
     }
--- server/Router/auth-router.js
+++ server/Router/auth-router.js
@@ -3,7 +3,7 @@ const router= express.Router();
 const authControllers = require("../Controllers/auth-controller");
 
 router.route("/").get(authControllers.home);
-router.route("/register").get(authControllers.register);
+router.route("/register1").post(authControllers.register);
 
 
 module.exports = router;
