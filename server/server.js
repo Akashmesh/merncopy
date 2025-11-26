9dc1d1f204d57cca5f7fbacdab42dcbdb784203e
Akashmesh <akashmeshram2486@gmail.com>
2025-11-27 02:22:53 +0530

    till login path

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const router =require("./router/auth-router");
 const connectDB = require("./utils/db");
-const router = require("./router/auth-router");
-
 app.use(express.json());
 app.use("/api/auth", router);
 
+
 const PORT = 5000;
-connectDB().then(()=> {
+connectDB().then (()=> {
     app.listen(PORT, () => {
-        console.log("app is listening on port", PORT);
+        console.log("app is listening on port ", PORT);
     })
-});
+})
--- server/utils/db.js
+++ server/utils/db.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
-const URI= process.env.MONGODB_URI;
+const URI = process.env.MONGODB_URI;
 
-const connectDB = async () => {
+const connectDB = async ()=> {
     try {
         await mongoose.connect(URI);
-        console.log("connection successful to db");
+        console.log("database connection created successfully");
     } catch (error) {
         console.error("database connection failed");
         process.exit(0);
